4dddab065d3b3cfb0957cba6cadd9db329172538
AJ Nielsen <ajniels95@gmail.com>
2023-09-17 00:18:00 -0600

    solution

--- tests.cs
+++ tests.cs
@@ -1,35 +1,72 @@
-// Output a variable that combines two strings together
-    // Console.WriteLine();
+/* Output a variable that combines two strings together
+string bob = "Saget" + " Bundy";
+ string ted = "Bundy";
+var lastNames = bob;
+    Console.WriteLine(lastNames);*/
+
 
 // Fix the issues below:
-    // int age = "5";
-    // inst age = 8
-    // string weight = 265;
+// int age = 5;
+// int age = 8;
+// string weight = "265";
 
 // Complete the if statment that checks if testOne is greater thatn testTwo
-    // int testOne = 5;
-    // int testTwo = 10;
-    // if() {
-    //     Console.WriteLine("It is greater!");
-    // }
+//int testone = 11;
+//int testtwo = 10;
+//if (testone > testtwo)
+//{
+//Console.WriteLine("It is greater!");
+//}
+//else
+//{
+//Console.WriteLine("It is less!");
+//}
 
 // Complete the for loop to log out all of the values in the array
-    // string[] stringArr = {"Yo", "he", "Ya", "wa"};
-    // for (int i = 0; i < stringArr.Count; i++) {
-    //     Console.WriteLine();
-    // }
+//string[] stringArr = { "Yo", "he", "Ya", "wa" };
+//for (int i = 0; i < stringArr.Length; i++)
+//{
+//    Console.WriteLine(stringArr[i]);
+//}
 
 // Complete the for each to log out all of the values in the array
-    // string[] stringArrTwo = {"Yo", "he", "Ya", "wa"};
-    // foreach (string i in stringArrTwo) {
-    //     Console.WriteLine();
-    // }
+//string[] stringArrTwo = { "Yo", "He", "Ya", "Wa" };
+//foreach (string i in stringArrTwo)
+//{
+//    Console.WriteLine(i);
+//}
 
 // Fizz Buzz Challenge: 
-    // Loop through this array of random number.
-    // If the number is a multiple of 3, write out Fizz
-    // If the number is a multiple of 5, write out Buzz
-    // If the number is a multiple of 3 and 5, wright out Fizz Buzz
-    // int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+// Loop through this array of random number.
+// If the number is a multiple of 3, write out Fizz
+// If the number is a multiple of 5, write out Buzz
+// If the number is a multiple of 3 and 5, write out Fizz Buzz
 
 
+int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+foreach (int i in arr)
+{
+    if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+
+    if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fizz Buzz");
+    }
+
+    if (i % 3 != 0 && i % 5 != 0) 
+    {
+        Console.WriteLine(i);
+    }
+}
+
+//int modulus = 5 % 3;
+//bool fizz = modulus == 2;
+//Console.WriteLine(fizz);
