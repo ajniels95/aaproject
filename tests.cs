32c0f8a2cda9e7e670683083b47133fa15f297bb
antianoj12763 <antiano.johnson@gmail.com>
2023-09-16 21:50:32 -0600

    Adding test

---
+++ tests.cs
@@ -0,0 +1,35 @@
+// Output a variable that combines two strings together
+    // Console.WriteLine();
+
+// Fix the issues below:
+    // int age = "5";
+    // inst age = 8
+    // string weight = 265;
+
+// Complete the if statment that checks if testOne is greater thatn testTwo
+    // int testOne = 5;
+    // int testTwo = 10;
+    // if() {
+    //     Console.WriteLine("It is greater!");
+    // }
+
+// Complete the for loop to log out all of the values in the array
+    // string[] stringArr = {"Yo", "he", "Ya", "wa"};
+    // for (int i = 0; i < stringArr.Count; i++) {
+    //     Console.WriteLine();
+    // }
+
+// Complete the for each to log out all of the values in the array
+    // string[] stringArrTwo = {"Yo", "he", "Ya", "wa"};
+    // foreach (string i in stringArrTwo) {
+    //     Console.WriteLine();
+    // }
+
+// Fizz Buzz Challenge: 
+    // Loop through this array of random number.
+    // If the number is a multiple of 3, write out Fizz
+    // If the number is a multiple of 5, write out Buzz
+    // If the number is a multiple of 3 and 5, wright out Fizz Buzz
+    // int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+
+
